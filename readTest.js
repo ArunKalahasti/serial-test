b71f13199a02f985e5a7a4ddb5d919b1b50ff5f2
Arun Kalahasti <arun.kalahasti@mavs.uta.edu>
2017-02-11 21:57:16 +0000

    partially working read test

---
+++ readTest.js
@@ -0,0 +1,22 @@
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/ttyACM0", {
+  baudRate: 9600
+});
+
+port.on('open', function() {
+  port.write('main screen turn on', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
+  });
+});
+
+// open errors will be emitted as an error event 
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+port.on('data', function (data) {
+  console.log('Data: ' + data);
+});
